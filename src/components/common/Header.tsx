bfa912d04641daa6399985424ef5d2c1f20e63ec
nishuldhakar <nishuldhakar.se@gmail.com>
2025-08-30 00:18:01 +0530

    added protected route

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
           ))}
         </nav>
         <div className="flex items-center gap-2">
-              {/* {!user && (
+              {!user && (
         <Link href="/register">
       <Button
         variant="outline"
@@ -54,7 +54,7 @@ export default function Navbar() {
         <span>Sign In</span>
       </Button>
     </Link>
-          )} */}
+          )}
           <GitHubStars />
       
         </div>
