3191471f30f5b8c5f16a25b94f3b12b992cbc686
nishuldhakar <nishuldhakar.se@gmail.com>
2025-08-29 08:12:22 +0530

    added auth

--- src/actions/google-auth-action.ts
+++ src/actions/google-auth-action.ts
@@ -3,6 +3,6 @@ import { authClient } from "@/lib/auth-client";
 export const signInWithGoogle = () => {
   authClient.signIn.social({
     provider: "google",
-    redirectTo: "/" 
+    callbackURL: "/" 
   });
 };
