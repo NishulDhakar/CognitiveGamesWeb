fa5f9f47dda4da50ff0cafcd7f5fa0e46b286929
nishuldhakar <nishuldhakar.se@gmail.com>
2025-08-30 00:32:14 +0530

    added protected route

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,21 +3,20 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
 export const auth = betterAuth({
-   database: prismaAdapter(prisma, {
-      provider: "postgresql"
-   }),
-   emailAndPassword: {
-      enabled: true,
-      autoSignIn: false
-   },
-   socialProviders: {
-      google: {
-         clientId: process.env.GOOGLE_CLIENT_ID as string,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      },
-   },
-   rateLimit: {
-      window: 60, 
-      max: 10,
-   },
-})
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  rateLimit: {
+    window: 60,
+    max: 10,
+  },
+});
