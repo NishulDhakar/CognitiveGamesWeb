113dfdea287cbad3397bc00e272d60f52fa82daa
nishuldhakar <nishuldhakar.se@gmail.com>
2025-08-30 09:05:59 +0530

    updated db url

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,12 +8,26 @@ import GitHubStars from "../Landing/GithubStar";
 import { usePathname } from "next/navigation";
 import { useUser } from "@/context/UserContext";
 import { Button } from "../ui/button";
-import { LogIn } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage, } from "../ui/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "../ui/ui/dropdown-menu";
+import { signOut } from "@/actions/auth-actions";
+
 
 export default function Navbar() {
   const pathname = usePathname();
   const altText = pathname === "/" ? "Blync" : "Dashboard";
   const user = useUser();
+
+  const handleSignOut = async () => {
+    await signOut();
+    window.location.reload();
+  };
 
   return (
     <header className="border-b border-gray-700/60 backdrop-blur-sm sticky top-0 z-50">
@@ -40,23 +54,38 @@ export default function Navbar() {
             </Link>
           ))}
         </nav>
+
         <div className="flex items-center gap-2">
-              {!user && (
-        <Link href="/register">
-      <Button
-        variant="outline"
-        size="lg"
-        className="rounded-xl border border-black font-semibold transition-all duration-200 
-                   hover:bg-zinc-100 hover:text-black hover:shadow-md 
-                   dark:border-zinc-200 dark:bg-white dark:text-black dark:hover:bg-zinc-100"
-      >
-        <LogIn className="w-4 h-4 mr-2 stroke-black dark:stroke-white" />
-        <span>Sign In</span>
-      </Button>
-    </Link>
+          {!user ? (
+            <Link href="/register">
+              <Button
+                variant="outline"
+                size="lg"
+                className="rounded-xl border border-black font-semibold transition-all duration-200 
+                           hover:bg-zinc-100 hover:text-black hover:shadow-md 
+                           dark:border-zinc-200 dark:bg-white dark:text-black dark:hover:bg-zinc-100"
+              >
+                <LogIn className="w-4 h-4 mr-2 stroke-black dark:stroke-white" />
+                <span>Sign In</span>
+              </Button>
+            </Link>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src={user.avatar} alt={user.email} />
+                  <AvatarFallback>{user.email?.[0].toUpperCase()}</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent sideOffset={5}>
+                <DropdownMenuItem onSelect={handleSignOut}>
+                  <LogOut className="w-4 h-4 mr-2" />
+                  Sign Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           )}
           <GitHubStars />
-      
         </div>
       </div>
     </header>
