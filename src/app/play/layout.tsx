bb1d2b149f3cccd27581185f5b64d405f9272500
nishuldhakar <nishuldhakar.se@gmail.com>
2025-09-02 01:23:36 +0530

    added solution in Switchchallenge

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -9,17 +9,17 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await auth.api.getSession({
-  //   headers: await headers(),
-  // });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  // if (!session) {
-  //   return redirect("/register");
-  // }
+  if (!session) {
+    return redirect("/register");
+  }
 
-  // const user = session?.user;
+  const user = session?.user;
   return (
-    <UserProvider user={null}>
+    <UserProvider user={user}>
               <Header />
       <main className="flex-1 p-6">{children}</main>=
     </UserProvider>
