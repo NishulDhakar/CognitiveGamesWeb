038c495c1944dd054eadee5bfa964986c6cfc7d6
nishuldhakar <nishuldhakar.se@gmail.com>
2025-12-07 21:24:10 +0530

    fix: leaderboard result

--- src/app/Leaderboard/client.tsx
+++ src/app/Leaderboard/client.tsx
@@ -124,7 +124,7 @@ export default function LeaderboardClient({ data, gameId }: LeaderboardClientPro
                                 </div>
                             </TabsTrigger>
                             <TabsTrigger
-                                value="grid-challenge"
+                                value="switch-challenge"
                                 className="rounded-full px-4 py-2 text-sm sm:px-6 sm:py-3 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground transition-all duration-300 flex-1 sm:flex-none min-w-[20px] md:min-w-[100px]"
                             >
                                 <div className="flex items-center justify-center gap-2">
@@ -142,7 +142,7 @@ export default function LeaderboardClient({ data, gameId }: LeaderboardClientPro
                                 </div>
                             </TabsTrigger>
                             <TabsTrigger
-                                value="switch-challenge"
+                                value="deductive-challenge"
                                 className="rounded-full px-4 py-2 text-sm sm:px-6 sm:py-3 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground transition-all duration-300 flex-1 sm:flex-none min-w-[100px]"
                             >
                                 <div className="flex items-center justify-center gap-2">
